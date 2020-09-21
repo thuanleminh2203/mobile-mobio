b8d1fd54992596bebcfaa5851d1fbcb518b75177
Son Van Sy Hai (HOHN-IT) <sonvsh@deaura.com.vn>
2020-09-21 11:05:48 +0700

    aa

--- src/main/java/com/venesa/mobio/MobileMobio/entity/Survey.java
+++ src/main/java/com/venesa/mobio/MobileMobio/entity/Survey.java
@@ -28,7 +28,6 @@ public class Survey implements Serializable {
     @Column(name = "customer_code")
     private String customerCode;
 
-    ////
     @Column(name = "rating")
     private int rating;
 
